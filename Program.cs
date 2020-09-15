1137bc4b464bc8ae18339c7a8433a94e721e5ed6
Catherine Santos <santos23@usf.edu>
2020-09-15 14:36:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Author: Catherine Santos
+    Date: 9/15/2020
+    Comments: This C# Console application code demonstrates the use of
+              iterative statements after getting input from users. 
+*/
+
+
+using System;
+
+namespace Iteration_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
